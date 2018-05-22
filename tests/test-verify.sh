555cf7d34b4dc10d2460154aae9de0cd750d7f77
Adam Dyson <adam.dyson@outlook.com>
2018-05-23 01:33:32 +1000

    Added Ansible 2.4 support

--- tests/test-verify.sh
+++ tests/test-verify.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# Copyright (c) Ontic. (http://www.ontic.com.au). All rights reserved.
+# See the COPYING file bundled with this package for license details.
 
-# Verify the installed Redis version.
+# Verify the installed location.
+docker exec --tty ${container_id} env TERM=xterm which redis-server
+# Verify the installed version.
 docker exec --tty ${container_id} env TERM=xterm redis-server --version
-
-# Verify the Redis server information.
+# Verify the server information.
 docker exec --tty ${container_id} env TERM=xterm redis-cli info server | egrep "process_id|tcp_port|config_file"
